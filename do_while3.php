4cdc5c31052906ad4b6200259f696d4964e1920f
KMetel2 <kmetel@gmail.com>
2025-09-25 12:22:34 +0200

    25.09.2025

--- do_while3.php
+++ do_while3.php
@@ -1,7 +1,7 @@
 <?php
     $tab = [];
     $x = 10;
-    for ($i = 0; $i < 11; $i++) {
+    for ($i = 0; $i < 11 ; $i++) {
         $tab[$i] = $x;
         $x++;
         echo "$i element tablicy wynosi: " . $tab[$i] . "<br>";
